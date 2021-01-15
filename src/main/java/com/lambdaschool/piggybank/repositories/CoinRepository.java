5c0db0ded0a60114415ac33d207ea98d86782786
kayode <kayode.richards@gmail.com>
2021-01-15 18:10:55 -0500

    test 2

--- src/main/java/com/lambdaschool/piggybank/repositories/CoinRepository.java
+++ src/main/java/com/lambdaschool/piggybank/repositories/CoinRepository.java
@@ -3,6 +3,7 @@ package com.lambdaschool.piggybank.repositories;
 import com.lambdaschool.piggybank.models.Coin;
 import org.springframework.data.repository.CrudRepository;
 
+//Coin repo
 public interface CoinRepository extends CrudRepository<Coin, Long>
 {
     boolean test (Coin c);
