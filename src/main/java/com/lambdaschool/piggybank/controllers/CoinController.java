b2b67c4440dd1f1468537feed7c1d24f7000c4b2
kayode <kayode.richards@gmail.com>
2021-01-15 18:31:12 -0500

    added application properties

--- src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -4,6 +4,7 @@ import com.lambdaschool.piggybank.models.Coin;
 import com.lambdaschool.piggybank.repositories.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
